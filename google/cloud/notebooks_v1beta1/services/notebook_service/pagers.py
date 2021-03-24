90fd1250aeb6ce8e2f3795952ea94a1d3430e5ae
yoshi-automation <yoshi-automation@google.com>
2021-03-23 18:40:43 -0700

    chore: update gapic-generator-python
    PiperOrigin-RevId: 355923884
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Fri Feb 5 14:04:52 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 5e3dacee19405529b841b53797df799c2383536c
    Source-Link: https://github.com/googleapis/googleapis/commit/5e3dacee19405529b841b53797df799c2383536c

--- google/cloud/notebooks_v1beta1/services/notebook_service/pagers.py
+++ google/cloud/notebooks_v1beta1/services/notebook_service/pagers.py
@@ -15,7 +15,16 @@
 # limitations under the License.
 #
 
-from typing import Any, AsyncIterable, Awaitable, Callable, Iterable, Sequence, Tuple
+from typing import (
+    Any,
+    AsyncIterable,
+    Awaitable,
+    Callable,
+    Iterable,
+    Sequence,
+    Tuple,
+    Optional,
+)
 
 from google.cloud.notebooks_v1beta1.types import environment
 from google.cloud.notebooks_v1beta1.types import instance
